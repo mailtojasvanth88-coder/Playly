a57093e8ffcf6e5ddedd484adbe5f918da6796fc
Jasvanth S <mailtojasvanth88-coder@users.noreply.github.com>
2025-09-25 15:21:07 +0530

    change router

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import PlayercomponentProvider, { Playercomponent } from './Components/Context/Playercomponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <PlayercomponentProvider>
     <App />
     </PlayercomponentProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
   
 )
