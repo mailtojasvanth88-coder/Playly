3830241f204e10ccdaad67e40637031021642864
Jasvanth S <mailtojasvanth88-coder@users.noreply.github.com>
2025-09-25 15:24:09 +0530

    change routes path

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -7,7 +7,7 @@ export default function Display() {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded-lg bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'> 
       <Routes>
-        <Route path="/Playly/" element={<DisplayHome />} />      
+        <Route path="/" element={<DisplayHome />} />      
         <Route path="Playly/album/:id" element={<DisplayAlbum />} /> 
       </Routes> 
     </div>
